a572732c1f98dcb4525c571abb651ec1fc9da096
Jakub Olszewski <j.olszewski1498@gmail.com>
2019-09-29 22:01:43 +0200

    Modified returned value to better describe data

--- src/services/impl/RpiTempStatusChecker.py
+++ src/services/impl/RpiTempStatusChecker.py
@@ -1,8 +1,11 @@
 import os
+import re
 
 from src.services.TempStatusChecker import TempStatusChecker
 
 
 class RpiTempStatusChecker(TempStatusChecker):
     def get_temp(self) -> dict:
-        return (os.popen("vcgencmd measure_temp").read()).strip()
+        output = os.popen("vcgencmd measure_temp").read().strip()
+        result = re.search(r"^(?:temp=)(\d+(?:\.\d|.*)+)'(.+)$", output)
+        return {"degrees": result.group(1), "unit": result.group(2)}
