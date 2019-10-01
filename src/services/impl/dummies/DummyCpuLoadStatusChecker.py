6271d0f2493252bce32ab190e0f79983265b13a8
Jakub Olszewski <j.olszewski1498@gmail.com>
2019-10-01 10:21:23 +0200

    Moved

--- src/services/impl/dummies/DummyCpuLoadStatusChecker.py
+++
@@ -1,7 +0,0 @@
-from src.services.CpuLoadStatusChecker import CpuLoadStatusChecker
-
-
-class DummyCpuLoadStatusChecker(CpuLoadStatusChecker):
-
-    def get_avg_cpu_load(self) -> dict:
-        return {'avg_1min': 0.00, 'avg_5min': 0.0, 'avg_10min': 0.0, 'processing_units': 1}
--- src/services/impl/dummies/DummyMemoryStatusChecker.py
+++
@@ -1,6 +0,0 @@
-from src.services.MemoryStatusChecker import MemoryStatusChecker
-
-
-class DummyMemoryStatusChecker(MemoryStatusChecker):
-    def get_mem_usage(self):
-        return {"mem": {"used": "2Gib"}}
