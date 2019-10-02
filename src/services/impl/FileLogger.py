e20a9c25045aa9207a52c89a3b21e8f619082844
Jakub Olszewski <j.olszewski1498@gmail.com>
2019-10-02 14:56:26 +0200

    Fixed data type for writing to file. (dict -> str)

--- src/services/impl/FileLogger.py
+++ src/services/impl/FileLogger.py
@@ -6,11 +6,11 @@ from src.services.Logger import Logger
 class FileLogger(Logger):
     fileHandler: TextIO
 
-    def __init__(self, fileName="logs.csv"):
-        self.fileHandler = open(fileName, "a+")
+    def __init__(self, configs, filePath=None):
+        self.fileHandler = open(filePath if filePath is not None else configs.file_config['path'], "a+")
 
     def log(self, data: any):
-        self.fileHandler.write(data)
+        self.fileHandler.write(str(data)+"\n")
         self.fileHandler.flush()
 
     def __del__(self):
